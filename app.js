0ea8410c0217d4e73c324e215c3ccc3210ed8686
0o9i8u7y6t <edgar.khachatryan95@mail.ru>
2018-04-07 16:17:01 +0300

    bgjsdc

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
 var express = require('express');
+const fortune = require("./lib/fortune");
 var app = express();
 
+//handlebars
 var handlebars = require('express-handlebars')
- .create({ defaultLayout:'main' });
-
+    .create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+//port
 app.set('port', process.env.PORT || 3000);
+//statics
+app.use(express.static(__dirname + '/public'));
 
+//routes
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.type('text/html');
+    res.render('home');
 });
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('/about');
+    res.type('text/html');
+    console.log(req.ip)
+    res.render('about', {fortune : fortune.getFortune()});
 });
-
-app.get('/about*', function (req, res) {
-    res.send('/about*');
-})
-app.get('/about/contact', function (req, res) {
-    res.send('/about/contact');
-})
-app.get('/about/directions', function (req, res) {
-    res.send('/about/directions');
-})
 
 // пользовательская страница 404
 app.use(function (req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 — Не найдено');
+    res.type('text/html');
+    res.render('404');
 });
 // пользовательская страница 500
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 — Ошибка сервера');
+    res.type('text/html');
+    res.render('500');
 });
 app.listen(app.get('port'), function () {
     console.log('Express запущен на http://localhost:' +
